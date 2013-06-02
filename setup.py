494f57b7fe78854af0d46634f22e14a16f3d4b57
Les Aker <me@lesaker.org>
2013-06-02 11:12:32 -0400

    added packagemagics

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+    name='modlib',
+    version='0.1.0',
+    description='Dynamic module loader',
+    author='Les Aker',
+    author_email='me@lesaker.org',
+    url='https://github.com/akerl/modlib',
+    py_modules=['modlib'],
+)
+
