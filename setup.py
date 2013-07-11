03232151e17f65f5cbcdea9d45ddab8bfa8124ad
Les Aker <me@lesaker.org>
2013-07-10 21:15:55 -0400

    bumped version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='modlib',
-    version='0.1.1',
+    version='0.1.2',
     description='Dynamic module loader',
     author='Les Aker',
     author_email='me@lesaker.org',
