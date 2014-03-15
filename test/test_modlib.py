8ce6288d1adbd9819ae71c8f8f72cf509fd3ea7a
Les Aker <me@lesaker.org>
2014-03-15 15:17:58 -0400

    test coverage

--- test/test_modlib.py
+++ test/test_modlib.py
@@ -1,7 +1,28 @@
 import unittest
 import modlib
+import sys
+import os
 
 
 class TestModlib:
-    def test_dummy(self):
-        pass
+    def test_modstack(self):
+        stack = modlib.Modstack()
+        names = ['apple', 'orange', 'tomato']
+        for i in [1, 2]:
+            modules = {x: stack.get('mock.' + x) for x in names}
+            for x in names:
+                assert modules[x].name() == x
+
+    def test_formula(self):
+        stack = modlib.Modstack(formula='mock.{name}')
+        names = ['apple', 'orange', 'tomato']
+        modules = {x: stack.get(name=x) for x in names}
+        for x in names:
+            assert modules[x].name() == x
+
+    def test_target(self):
+        stack = modlib.Modstack(target='name')
+        names = ['apple', 'orange', 'tomato']
+        modules = {x: stack.get('mock.' + x) for x in names}
+        for x in names:
+            assert modules[x]() == x
