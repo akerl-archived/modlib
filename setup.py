7811044cfd1efa109982ab87ee405db09ba78e00
Les Aker <me@lesaker.org>
2015-01-19 23:15:49 -0500

    release v1.0.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='modlib',
-    version='0.1.5',
+    version='1.0.0',
     description='Dynamic module loader',
     author='Les Aker',
     author_email='me@lesaker.org',
